377e9f18a0f30c232bf5fe4e13245aead622ac57
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-05-31 00:13:23 +0300

    Implement N-Queens problem solver with backtracking algorithm

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-N-Queens Problem Solver
+N queens
 """
 
 import sys
@@ -12,25 +12,16 @@ if len(sys.argv) != 2:
 try:
     n_q = int(sys.argv[1])
 except ValueError:
-    print('N must be a number')
+    print('N must ba a number')
     exit(1)
 
 if n_q < 4:
-    print('N must be at least 4')
+    print('N must ba at least 4')
     exit(1)
 
 
 def solve_nqueens(n):
-    """
-    Solve the N-Queens problem using backtracking.
-
-    Args:
-        n (int): The number of queens and the size of the board (n x n).
-
-    Returns:
-        list: A list of solutions, where each solution is a list of tuples
-              representing the positions of queens on the board.
-    """
+    ''' self descriptive '''
     if n == 0:
         return [[]]
     inner_solution = solve_nqueens(n - 1)
@@ -41,34 +32,15 @@ def solve_nqueens(n):
 
 
 def attack_queen(square, queen):
-    """
-    Determine if two queens attack each other.
-
-    Args:
-        square (tuple): The position of the first queen (row, column).
-        queen (tuple): The position of the second queen (row, column).
-
-    Returns:
-        bool: True if the queens attack each other, False otherwise.
-    """
+    '''self descriptive'''
     (row1, col1) = square
     (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or
-            abs(row1 - row2) == abs(col1 - col2)
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
 
 
 def safe_queen(sqr, queens):
-    """
-    Check if a queen can be safely placed at a given position.
-
-    Args:
-        sqr (tuple): The position to check (row, column).
-        queens (list): A list of positions of queens already placed
-                       on the board.
-
-    Returns:
-        bool: True if the position is safe, False otherwise.
-    """
+    '''self descriptive'''
     for queen in queens:
         if attack_queen(sqr, queen):
             return False
